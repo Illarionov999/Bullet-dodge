c63612d91be08176c3fbfc1f9dbe6ccc94065dbd
Illarionov999 <artemillarionov.personal@gmail.com>
2025-11-05 00:06:16 -0500

    Created window, background, and player model

---
+++ main.py
@@ -0,0 +1,38 @@
+import pygame
+import time
+import random
+
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Bullet Dodge")
+
+BG = pygame.transform.scale(pygame.image.load("imgs/bg.jpeg"), (WIDTH, HEIGHT))
+
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+
+def draw(player):
+    WIN.blit(BG, (0, 0))
+
+    pygame.draw.rect(WIN, "red", player) # or use RGB "(255, 0, 0)" instead of ""red""
+
+    pygame.display.update()
+
+def main():
+    run = True
+
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        draw(player)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
